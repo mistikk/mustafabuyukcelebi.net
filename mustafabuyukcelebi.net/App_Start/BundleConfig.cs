8ad9ec3e2983829eab6fd3bc2ecb4087a0798d2c
Mustafa Büyükçelebi <mustafa@polisoft.com.tr>
2016-01-16 14:59:46 +0200

    masterpage düzenlendi.

--- mustafabuyukcelebi.net/App_Start/BundleConfig.cs
+++ mustafabuyukcelebi.net/App_Start/BundleConfig.cs
@@ -9,10 +9,19 @@ namespace mustafabuyukcelebi.net
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/plugins/jquery/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/plugins/jquery/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/plugins/jquery/jquery-ui/jquery-ui.js"));
+
+            bundles.Add(new ScriptBundle("~/Scripts/plugins/jquery-ui/jqueryuiStyles").Include(
+                        "~/Scripts/plugins/jquery/jquery-ui/jquery-ui.css"));
+
+            bundles.Add(new ScriptBundle("~/plugins/slimScroll").Include(
+                      "~/Scripts/plugins/slimscroll/jquery.slimscroll.min.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -23,9 +32,35 @@ namespace mustafabuyukcelebi.net
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                      "~/Scripts/plugins/angularjs/angular.min.js",
+                      "~/Scripts/plugins/angularjs/angular-resource.min.js",
+                      "~/Scripts/plugins/angularjs/angular-route.min.js",
+                      "~/Scripts/Angular/*.js"));
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/font-awesome/css").Include(
+                      "~/fonts/font-awesome/css/font-awesome.min.css", new CssRewriteUrlTransform()));
+
+            // Inspinia skin config script
+            bundles.Add(new ScriptBundle("~/bundles/skinConfig").Include(
+                      "~/Scripts/app/skin.config.min.js"));
+
+            // Inspinia script
+            bundles.Add(new ScriptBundle("~/bundles/inspinia").Include(
+                      "~/Scripts/plugins/metisMenu/metisMenu.min.js",
+                      "~/Scripts/plugins/pace/pace.min.js",
+                      "~/Scripts/app/inspinia.min.js"));
+
+            // CSS style (bootstrap/inspinia)
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/animate.css",
+                      "~/Content/style.css"));
+
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
---
+++ mustafabuyukcelebi.net/Scripts/Angular/controller.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app', ['ngRoute'])
+        .controller('controller', controller);
+
+    controller.$inject = ['$scope']; 
+
+    function controller($scope) {
+        $scope.title = 'MİSTİK';
+
+        activate();
+
+        function activate() { }
+    }
+})();
